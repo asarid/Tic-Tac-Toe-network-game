5f3ab3a1e266484cdc6e963d0c2fa1bb151bc41e
asarid <aviad623@gmail.com>
2024-03-29 13:15:33 +0300

    Delete runServer0.bat

--- runServer0.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Users\aviad\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\aviad\Projects\Courses\InternetNetworking_5784\TicTacToe\Server\DataServiceServer\appServer.py"
-
-pause
